89eea756fcd1df2ffc4b1ad82e93b10b9a61140a
FerryLa <rlawl4240@gmail.com>
2025-06-07 16:07:47 +0900

    fix "login"

---
+++ src/main/java/com/ohgiraffers/gitflow/task/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.gitflow.task.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "로그인 기능";
+    }
+}
