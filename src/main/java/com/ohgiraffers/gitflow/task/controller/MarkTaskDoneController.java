552d20ca8e3743d447e4cf0cd4dfbe048a9e1c92
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-07 15:33:45 +0900

    feature: mark-task-done 개발환경 테스트

---
+++ src/main/java/com/ohgiraffers/gitflow/task/controller/MarkTaskDoneController.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.gitflow.task.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/task")
+public class MarkTaskDoneController {
+
+    @GetMapping("mark-task")
+    public String markTask() {
+        return "할일 완료처리하는 기능 개발 진행중";
+    }
+
+}
