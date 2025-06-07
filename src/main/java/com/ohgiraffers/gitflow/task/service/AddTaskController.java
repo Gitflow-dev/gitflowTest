67997f0b8f76d5cd6337f0e5262e7f30965636dc
juhui <juya202106@naver.com>
2025-06-07 16:27:03 +0900

    add task done

---
+++ src/main/java/com/ohgiraffers/gitflow/task/service/AddTaskController.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.gitflow.task.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddTaskController {
+   public void addTaskDone(){
+       System.out.println("add task done");
+   }
+}
