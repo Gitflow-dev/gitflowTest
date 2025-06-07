b23f35d37439532b33763d6a452f3766c019726b
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-07 15:21:26 +0900

    first commit

---
+++ src/test/java/com/ohgiraffers/gitflow/GitflowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
